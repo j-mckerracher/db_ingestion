7acd2fc31b640bb6fdfadb7f3d2362a2d6e0ed0b
josh <jmckerra@purdue.edu>
2023-07-10 15:37:02 -0600

    script updated to use copy_from

--- ingest_one_file_with_copy.py
+++ ingest_one_file_with_copy.py
@@ -3,44 +3,36 @@ from datetime import datetime
 import psycopg2
 import pytz
 
-connection = psycopg2.connect(f"host=frescodb, user=admin, password={os.getenv('DBPW')}")
+# Establish a connection to the PostgreSQL database
+conn = psycopg2.connect(host="frescodb", dbname="anvil", user="admin", password=f"{os.getenv('DBPW')}")
 
-cursor = connection.cursor()
+# Create a cursor object
+cur = conn.cursor()
+print(conn.get_dsn_parameters(), "\n")
 
-# Folder path containing the CSV files
-folder_path = "time_series"
-
-timezone = pytz.timezone('MST')
-
-filename = r"job_ts_metrics_july2022_anon.csv"
+filename = "job_ts_metrics_july2022_anon.csv"
 
 try:
-    print(connection.get_dsn_parameters(), "\n")
-    file_path = os.path.join(folder_path, filename)
-    current_time = datetime.now(timezone)
-    formatted_time = current_time.strftime("%H:%M:%S")
-    print(f"Started working on {filename} at {formatted_time}.")
+    # Open the CSV file
+    with open(filename, 'r') as f:
+        # Skip the header row
+        next(f)
 
-    # Execute COPY command
-    copy_sql = """
-        COPY host_data(jid, host, event, value, unit, time) 
-        FROM STDIN WITH CSV HEADER DELIMITER as ','
-    """
-    file = open(file_path, 'r')
-    cursor.copy_expert(sql=copy_sql, file=file)
-    connection.commit()
-    file.close()
+        # time stuff
+        timezone = pytz.timezone('MST')
+        current_time = datetime.now(timezone)
+        formatted_time = current_time.strftime("%H:%M:%S")
 
-    # Print PostgreSQL version
-    cursor.execute("SELECT version();")
-    record = cursor.fetchone()
-    print("You are connected to - ", record, "\n")
+        print(f"Started working on {filename} at {formatted_time}.")
 
+        # Use copy_from to run the COPY command
+        cur.copy_from(f, 'host_data', columns=('jid', 'host', 'event', 'value', 'unit', 'time'), sep='\t', null="")
+
+    conn.commit()
 except (Exception, psycopg2.Error) as error:
     print("Error while connecting to PostgreSQL", error)
 finally:
-    # closing database connection.
-    if connection:
-        cursor.close()
-        connection.close()
+    if conn:
+        cur.close()
+        conn.close()
         print("PostgreSQL connection is closed")
