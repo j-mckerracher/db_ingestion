6848ba4c9a762eab815f949cd5eafcf6fe99ad5d
josh <jmckerra@purdue.edu>
2023-07-14 10:42:00 -0600

    Troubleshooting

--- ingest_job_accounting.py
+++ ingest_job_accounting.py
@@ -3,77 +3,84 @@ import pandas as pd
 import psycopg2
 from psycopg2 import extras
 
-# Load the CSV file
-df = pd.read_csv('/mnt/data/JobAccounting/job_accounting_sep2022_anon.csv')
-
-# Drop the unnecessary columns
-df = df.drop(columns=['Shared', 'Cpu Time', 'Node Time', 'Requested Nodes', 'Wait Time', 'Wall Time', 'Eligible Time'])
-
-# Rename the columns to match the database schema
-df = df.rename(columns={
-    'Account': 'account',
-    'Job Id': 'jid',
-    'Cores': 'ncores',
-    'Gpus': 'ngpus',
-    'Nodes': 'nhosts',
-    'Requested Wall Time': 'timelimit',
-    'Queue': 'queue',
-    'End Time': 'end_time',
-    'Start Time': 'start_time',
-    'Submit Time': 'submit_time',
-    'User': 'username',
-    'Exit Status': 'exitcode',
-    'Hosts': 'host_list',
-    'Job Name': 'jobname'
-})
-
-# Convert 'Hosts' to a list
-df['host_list'] = df['host_list'].str.split(',')
-
-# Convert timestamp columns to the appropriate format
-df['end_time'] = pd.to_datetime(df['end_time']).dt.strftime('%Y-%m-%d %H:%M:%S')
-df['start_time'] = pd.to_datetime(df['start_time']).dt.strftime('%Y-%m-%d %H:%M:%S')
-df['submit_time'] = pd.to_datetime(df['submit_time']).dt.strftime('%Y-%m-%d %H:%M:%S')
-
-print(df.head())
-# Order the DataFrame columns to match the PostgreSQL table
-df = df[['jid', 'submit_time', 'start_time', 'end_time',
-         'timelimit', 'nhosts', 'ncores', 'ngpus',
-         'username', 'account', 'queue', 'jobname',
-         'exitcode', 'host_list']]
-
-
 # Connect to the PostgreSQL database
 conn = psycopg2.connect(host="frescodb", dbname="anvil", user="admin", password=f"{os.getenv('DBPW')}")
 cursor = conn.cursor()
 
 print("Connected to the database")
 
-# Prepare the INSERT statement
-insert_sql = """
-    INSERT INTO job_data (
-        jid, submit_time, start_time, end_time, 
-        timelimit, nhosts, ncores, ngpus, 
-        username, account, queue, jobname, 
-        exitcode, host_list
-    )
-    VALUES %s
-"""
+job_accounting_directory_path = "/mnt/data/JobAccounting"
 
-print("Preparing the INSERT statement")
+for filename in os.listdir(job_accounting_directory_path):
+    if filename.endswith(".csv") and "job_accounting_sep2022_anon" not in filename:
+        file_path = os.path.join(job_accounting_directory_path, filename)
 
-# Prepare the data to be inserted
-data_to_insert = list(df.itertuples(index=False, name=None))
+        print(f'Starting on {filename}')
 
-print("Inserting data")
+        # Load the CSV file
+        df = pd.read_csv(file_path)
 
-# Use psycopg2.extras.execute_values() to insert the data
-extras.execute_values(cursor, insert_sql, data_to_insert)
+        # Drop the unnecessary columns
+        df = df.drop(columns=['Shared', 'Cpu Time', 'Node Time', 'Requested Nodes', 'Wait Time', 'Wall Time', 'Eligible Time'])
 
-# Commit the transaction
-conn.commit()
+        # Rename the columns to match the database schema
+        df = df.rename(columns={
+            'Account': 'account',
+            'Job Id': 'jid',
+            'Cores': 'ncores',
+            'Gpus': 'ngpus',
+            'Nodes': 'nhosts',
+            'Requested Wall Time': 'timelimit',
+            'Queue': 'queue',
+            'End Time': 'end_time',
+            'Start Time': 'start_time',
+            'Submit Time': 'submit_time',
+            'User': 'username',
+            'Exit Status': 'exitcode',
+            'Hosts': 'host_list',
+            'Job Name': 'jobname'
+        })
 
-print("Data committed")
+        # Convert 'Hosts' to a list
+        df['host_list'] = df['host_list'].str.split(',')
+
+        # Convert timestamp columns to the appropriate format
+        df['end_time'] = pd.to_datetime(df['end_time']).dt.strftime('%Y-%m-%d %H:%M:%S')
+        df['start_time'] = pd.to_datetime(df['start_time']).dt.strftime('%Y-%m-%d %H:%M:%S')
+        df['submit_time'] = pd.to_datetime(df['submit_time']).dt.strftime('%Y-%m-%d %H:%M:%S')
+
+        print(df.head())
+        # Order the DataFrame columns to match the PostgreSQL table
+        df = df[['jid', 'submit_time', 'start_time', 'end_time',
+                 'timelimit', 'nhosts', 'ncores', 'ngpus',
+                 'username', 'account', 'queue', 'jobname',
+                 'exitcode', 'host_list']]
+
+        # Prepare the INSERT statement
+        insert_sql = """
+            INSERT INTO job_data (
+                jid, submit_time, start_time, end_time, 
+                timelimit, nhosts, ncores, ngpus, 
+                username, account, queue, jobname, 
+                exitcode, host_list
+            )
+            VALUES %s
+        """
+
+        print("Preparing the INSERT statement")
+
+        # Prepare the data to be inserted
+        data_to_insert = list(df.itertuples(index=False, name=None))
+
+        print(f"Inserting data for {filename}")
+
+        # Use psycopg2.extras.execute_values() to insert the data
+        extras.execute_values(cursor, insert_sql, data_to_insert)
+
+        # Commit the transaction
+        conn.commit()
+
+        print(f"Data committed for {filename}")
 
 # Close the cursor and connection
 cursor.close()
